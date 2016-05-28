3ee69fb9aaa94a24b3a614f6fdce24f930ff1f43
rubangarmon <rubangarmon99@gmail.com>
2016-05-28 15:01:39 -0600

    first

---
+++ 2016-2-Groupware-master/Groupware.Calentamiento/Core.Numero/Numero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Rub'en
+namespace Core.Numero
+{
+    public class Numero
+    {
+        public string elNumero { get; set; }
+        public int laBase { get; set; }
+
+        public bool esBaseDiez { get {
+                return laBase == 10; }
+             }
+
+        public double elNumeroEnDecimal { get {
+                return double.Parse(elNumero); } }
+
+    }
+}
